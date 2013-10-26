dd95adaa03e27aeccd61282b5d6663b599ef4247
letusfly85 <letusfly95@gmail.com>
2013-10-26 09:56:13 +0900

    add setter

--- src/main/scala/com/jellyfish85/svnaccessor/bean/SVNRequestBean.scala
+++ src/main/scala/com/jellyfish85/svnaccessor/bean/SVNRequestBean.scala
@@ -12,4 +12,16 @@ class SVNRequestBean {
 
   var exist: Boolean = _
 
+  def setPath(_path: String) =  path = _path
+
+  def setRevision(_revision: Long) = revision = _revision
+
+  def setFileName(_fileName: String) = fileName = _fileName
+
+  def setFileData(_fileData: Array[Byte]) = fileData = _fileData
+
+  def setOldData(_oldData: Array[Byte]) = oldData = _oldData
+
+  def setExist(_exist: Boolean) = exist = _exist
+
 }
